6948bb00f6270f7e8f9f43fa533408f4efd07429
Viacheslav Silchenko <122287358+LckBastrd@users.noreply.github.com>
2024-05-26 17:46:26 +0200

    Initial commit

---
+++ app/src/main/java/com/example/hw3/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.example.hw3
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.hw3.ui.theme.HW3Theme
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detalies)
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    HW3Theme {
+        Greeting("Android")
+    }
+}
